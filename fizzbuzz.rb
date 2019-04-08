0b4589db5f49803136a40f0848d6223d976b4171
Francis Mendez <fmendez884@gmail.com>
2019-04-08 18:48:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 3 == 0 && int % 5 == 0 
+    return "FizzBuzz"int % 3 == 0
   elsif int % 5 == 0 
     return "Buzz"
   elsif int % 3 == 0 && int % 5 == 0 
