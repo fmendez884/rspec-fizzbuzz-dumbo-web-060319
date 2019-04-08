e1821b98d6b4667dee7ac10b9d863d11397ff1c6
Francis Mendez <fmendez884@gmail.com>
2019-04-08 18:35:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(n)
+def fizzbuzz
   if n / 3 == 1 
     return "Fizz"
   elsif n / 5 == 1 
